0c487e0891199016ed5c15d85f9e43dfeecdb3ca
ArtHitect <Oratilediale11122004@gmail.com>
2023-06-21 05:19:55 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*ok
+/**
 * main - Entry point
 * Return: Always 0
 */
@@ -12,12 +12,12 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-	_putchar('t'); 
-	_putchar('c'); 
+	_putchar('t');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n'); 
+	_putchar('\n');
 	return (0);
 
 }
