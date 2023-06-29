fafcf8321115e3d84172a39cb1ee44cd1608758f
ArtHitect <Oratilediale11122004@gmail.com>
2023-06-29 06:03:51 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,15 @@
 * @src: String to be concatenated upon
 * Return: returns poiner to @dest
 */
-char *_strcat (char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
-	while (dest [index++])
+	while (dest[index++])
 		dest_len++;
 
 	for (index = 0; src[index]; index++)
-	dest [dest_len++] = src[index];
+	dest[dest_len++] = src[index];
 
 	return (dest);
 }
